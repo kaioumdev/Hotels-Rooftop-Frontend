e311e4f766b57b677b60bd96d524ca38573721db
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-29 23:13:38 +0600

    create home page attach with app folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <navbar>
+        <h1>Header</h1>
+      </navbar>
+      <Outlet></Outlet>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
     </>
   )
 }
