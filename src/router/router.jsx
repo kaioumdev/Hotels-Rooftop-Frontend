4fc72fe2415468b9e506801f687101fbcbcc0488
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-29 22:38:27 +0600

    create router.jsx file to route file

---
+++ src/router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <>Hello World</>
+    }
+])
+
+export default router
