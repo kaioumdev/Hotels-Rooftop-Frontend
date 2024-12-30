4f3cc3c03fb460c4b38aeb1297ec6b89d3e9773b
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-30 14:16:10 +0600

    dynamically use navlist

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const navLists = [
+    { name: "Home", path: "/" },
+    { name: "About Us", path: "/about-us" },
+    { name: "Privacy Policy", path: "/privacy-policy" },
+    { name: "Contact Us", path: "/contact-us" },
+]
 
 const Navbar = () => {
+
     return (
-        <header className='bg-white p-6 border'>
+        <header className='bg-white py-6 border'>
             <nav className='container mx-auto flex justify-between px-5'>
                 <a href="">
                     <img src="/logo.png" alt="" className='h-12' />
                 </a>
+                <ul className='sm:flex hidden items-center gap-8'>
+                    {
+                        navLists.map((item, index) => (
+                            <li>
+                                <NavLink to={`${item.path}`}>{item.name}</NavLink>
+                            </li>
+                        ))
+                    }
+                </ul>
             </nav>
         </header>
     )
 }
 
-export default Navbar
+export default Navbar;
