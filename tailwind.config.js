989675e7a240707abfb7e993504e7cfa6a14c0ca
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-30 21:05:56 +0600

    solve tailwind color issue to bg-color work successfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,29 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     colors: {
+//       bgPrimary: '#F7F8F9',
+//       primary: '#222222',
+//       accent: '#34495E',
+//     },
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      bgPrimary: '#F7F8F9',
-      primary: '#222222',
-      accent: '#34495E',
-    },
+    extend: {},
   },
   plugins: [],
 }
+
 
