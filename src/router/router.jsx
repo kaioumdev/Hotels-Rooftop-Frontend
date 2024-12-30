a74f95d3a9223e61f3ea68b502032957e948a88d
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-30 14:31:06 +0600

    complete router to connect pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom"
 import App from "../App"
 import Home from "../pages/home/Home"
+import About from "../pages/miniPage/About"
+import PrivacyPolicy from "../pages/miniPage/PrivacyPolicy"
+import ContactUs from "../pages/miniPage/ContactUs"
 
 const router = createBrowserRouter([
     {
@@ -10,6 +13,18 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>
+            },
+            {
+                path: "/about-us",
+                element: <About></About>
+            },
+            {
+                path: "/privacy-policy",
+                element: <PrivacyPolicy></PrivacyPolicy>
+            },
+            {
+                path: "/contact-us",
+                element: <ContactUs></ContactUs>
             }
         ]
     }
