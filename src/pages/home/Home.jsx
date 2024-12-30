e4ef57170f7cbd2ed737edc9f63c8263380fb3e9
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-30 23:06:22 +0600

    create blogs page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,11 @@
+import Blogs from "../blogs/Blogs";
 import Hero from "./Hero"
 
 const Home = () => {
     return (
         <div className="bg-white text-primary container mx-auto mt-8 p-8">
             <Hero></Hero>
-            <hr />
-            <div>
-                Blogs
-            </div>
+            <Blogs></Blogs>
         </div>
     )
 }
