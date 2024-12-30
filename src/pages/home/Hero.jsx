c8d226a897635bfca730b8228b979c438c0427ba
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-30 20:47:08 +0600

    updated hero section

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
                 <h1 className='md:text-5xl text-3xl font-bold md:leading-tight'>Hotels with Rooftop Pools Near Me</h1>
                 <p className='py-4'>Discovering hotels with rooftop pools near you! Whether youre planning a luxurious staycation or a weekend getaway, our curated selection of hotels with rooftop pools will help you beat the heat and elevate your travel experience.</p>
             </div>
-            <div>
+            <div className='md:w-1/2 w-full mx-auto bg-black'>
                 Image
             </div>
         </div>
