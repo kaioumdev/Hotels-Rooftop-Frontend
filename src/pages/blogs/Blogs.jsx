0231f2f3ef3ae742be0be9c0b2b534b6067e5058
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-31 17:58:46 +0600

    create searchBlog page & update blog page

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import SearchBlog from './SearchBlog'
 
 const Blogs = () => {
     return (
-        <div>Blogs</div>
+        <div className='mt-16 container mx-auto'>
+            <SearchBlog></SearchBlog>
+            <div>
+                Blog Card
+            </div>
+        </div>
     )
 }
 
