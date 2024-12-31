952e8d69023205c2cc594e70f77bfac839d46c98
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-31 22:54:34 +0600

    create a search functionality at blogs file

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBlog from './SearchBlog'
 
 const Blogs = () => {
+    const [search, setSearch] = useState("");
+    const [category, setCategory] = useState("");
+    const [query, seQuery] = useState({ search: "", category: "" });
+
+    const handleSearchChange = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const handleSearch = () => seQuery({ search, query });
     return (
         <div className='mt-16 container mx-auto'>
-            <SearchBlog></SearchBlog>
+            <SearchBlog
+                search={search}
+                handleSearchChange={handleSearchChange}
+                handleSearch={handleSearch}
+            ></SearchBlog>
             <div>
                 Blog Card
             </div>
