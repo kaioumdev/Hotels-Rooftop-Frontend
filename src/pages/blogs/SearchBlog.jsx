d7caabb3334a6a6e8d91aafddcc022d302350c13
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-01 00:31:45 +0600

    create handleKeyPress function

--- src/pages/blogs/SearchBlog.jsx
+++ src/pages/blogs/SearchBlog.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const SearchBlog = ({ search, handleSearchChange, handleSearch }) => {
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+            handleSearch()
+        }
+    }
     return (
         <div className='w-full flex'>
             <input type="text"
+                value={search}
+                onChange={handleSearchChange}
+                onKeyPress={handleKeyPress}
             />
             <button className='bg-[#1E73BE] px-4 py-2 text-white'>Search</button>
         </div>
