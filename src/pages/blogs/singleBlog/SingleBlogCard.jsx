4713698ff5511ca509abd15b97f25a25b8a9ad7c
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-01 17:19:25 +0600

    updated singleBlogCard to show title,image

--- src/pages/blogs/singleBlog/SingleBlogCard.jsx
+++ src/pages/blogs/singleBlog/SingleBlogCard.jsx
@@ -7,7 +7,10 @@ const SingleBlogCard = ({ blog }) => {
         <div className='bg-white p-8'>
             <div>
                 <h1 className='md:text-4xl text-3xl font-medium mb-4'>{title}</h1>
-                <p>{formatDate(createdAt)}</p>
+                <p className='mb-6'>{formatDate(createdAt)} by <span className='text-blue-400 cursor-pointer'>Admin 1</span></p>
+            </div>
+            <div>
+                <img src={coverImg} alt="Cover Image" className='w-full md:h-[520px] bg-cover' />
             </div>
         </div>
     )
