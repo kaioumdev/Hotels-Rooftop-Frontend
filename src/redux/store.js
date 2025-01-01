1d91be5ec41632b7a381a1118f0dd3fbf88d12e5
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-01 13:02:15 +0600

    use provider on the root folder connect with store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { blogApi } from "./features/blogs/blogsApi";
 
-const store = configureStore({
+export const store = configureStore({
     reducer: {
         [blogApi.reducerPath]: blogApi.reducer
     },
