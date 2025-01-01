31d41e411c26833c7170be19d091cfd61d999792
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-01 17:09:54 +0600

    implements formateDate function using blog date

--- src/pages/blogs/singleBlog/SingleBlog.jsx
+++ src/pages/blogs/singleBlog/SingleBlog.jsx
@@ -5,16 +5,16 @@ import SingleBlogCard from './SingleBlogCard';
 
 const SingleBlog = () => {
     const { id } = useParams();
-    // console.log(id);
+    console.log(id);
     const { data: blog, error, isLoading } = useFetchBlogsByIdQuery(id);
-    console.log(blog);
+    // console.log(blog);
     return (
         <div className='text-primary container mx-auto mt-8'>
             <div>
                 {isLoading && <di>Loading....</di>}
                 {error && <div>Something went worng...</div>}
                 {
-                    blog.post && (
+                    blog?.post && (
                         <div className='flex flex-col lg:flex-row justify-between items-start md:gap-12 gap-8'>
                             <div className='lg:w-2/3 w-full'>
                                 <SingleBlogCard blog={blog.post}></SingleBlogCard>
--- src/pages/blogs/singleBlog/SingleBlogCard.jsx
+++ src/pages/blogs/singleBlog/SingleBlogCard.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
+import { formatDate } from '../../../utils/formateDate';
 
 const SingleBlogCard = ({ blog }) => {
-    const { title, description, content, coverImg, category, rating, author } = blog || {};
-
-    const formatDate = (isDate) => {
-        const date = new Date(isDate);
-        return date.toLocaleDateString('en-US', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        });
-    }
+    const { title, description, content, coverImg, category, rating, author, createdAt } = blog || {};
     return (
-        <div>SingleBlogCard</div>
+        <div className='bg-white p-8'>
+            <div>
+                <h1 className='md:text-4xl text-3xl font-medium mb-4'>{title}</h1>
+                <p>{formatDate(createdAt)}</p>
+            </div>
+        </div>
     )
 }
 
