43e94e9347652dd3fa016bcf97ae4ababa64ed2a
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-02 11:47:22 +0600

    remove unusual code from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import tailwindcss from 'tailwindcss';
-// import react from '@vitejs/plugin-react';
-// import commonjs from 'vite-plugin-commonjs';
-
-// export default defineConfig({
-//   plugins: [react(), commonjs()],
-//   css: {
-//     postcss: {
-//       plugins: [tailwindcss()],
-//     },
-//   },
-//   optimizeDeps: {
-//     include: ['editorjs-html'], // Ensure this package is pre-bundled
-//   },
-//   build: {
-//     commonjsOptions: {
-//       include: [/editorjs-html/, /node_modules/], // Include CommonJS dependencies
-//     },
-//   },
-
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import commonjs from 'vite-plugin-commonjs';
