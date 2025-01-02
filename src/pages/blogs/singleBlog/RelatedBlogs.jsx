bc3b17c3068a1996435a898d3bc7ba13ce88b128
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-02 20:58:59 +0600

    finally got related blogs from api & solve the errors

--- src/pages/blogs/singleBlog/RelatedBlogs.jsx
+++ src/pages/blogs/singleBlog/RelatedBlogs.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useFetchRelatedBlogsQuery } from '../../../redux/features/blogs/blogsApi';
 
 const RelatedBlogs = () => {
+    const { id } = useParams();
+    const { data: blogs = [], error, isLoading } = useFetchRelatedBlogsQuery(id);
+    console.log(blogs)
     return (
         <div>
             <h3 className='text-2xl font-medium pt-8 px-8 pb-5'>Related Blogs</h3>
+            <hr />
+            {
+                blogs.length > 0 ? (<div className='p-8'>No Related Blogs Found</div>) : (<div className='space-y-4 mt-5'>
+                    {
+                        blogs.map((blog) => (
+                            <Link>
+                                <div>
+                                    <img src={blog.coverImg} alt="" />
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </div>)
+            }
         </div>
     )
 }
