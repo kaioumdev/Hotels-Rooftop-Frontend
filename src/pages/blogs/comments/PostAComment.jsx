44b5692afb5d4169bac158e34f832f8e3e471e03
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-02 18:32:04 +0600

    complete postAComment page & styles

--- src/pages/blogs/comments/PostAComment.jsx
+++ src/pages/blogs/comments/PostAComment.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const PostAComment = () => {
     return (
-        <div>PostAComment</div>
+        <div>
+            <h3 className='text-lg font-medium mb-8'>Leave a Comment</h3>
+            <form>
+                <textarea name="text" value={comment} onChange={(e) => setComment(e.target.value)} cols="30" rows="10" placeholder='Share your opinion about this post....'
+                    className='w-full bg-bgPrimary focus:outline-none p-5' id=""></textarea>
+                <button type='submit' className='w-full bg-primary hover:bg-indigo-500 text-white font-medium py-3 rounded-md'>Submit</button>
+            </form>
+        </div>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,30 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     colors: {
-//       bgPrimary: '#F7F8F9',
-//       primary: '#222222',
-//       accent: '#34495E',
-//     },
-//   },
-//   plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      bgPrimary: '#F7F8F9',
+      primary: '#222222',
+      accent: '#34495E',
+    },
     extend: {},
   },
   plugins: [],
 }
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 
 
