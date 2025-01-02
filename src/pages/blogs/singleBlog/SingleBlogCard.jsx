b4d5e406076257aa915546b7514d871fe525c3bc
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-02 15:24:11 +0600

    add rating to the singleBlogCard

--- src/pages/blogs/singleBlog/SingleBlogCard.jsx
+++ src/pages/blogs/singleBlog/SingleBlogCard.jsx
@@ -19,6 +19,11 @@ const SingleBlogCard = ({ blog }) => {
             {/* blog details */}
             <div className='mt-8 space-y-4'>
                 <div dangerouslySetInnerHTML={{ __html: htmlContent }} className='space-y-3 editorjsdiv' />
+                <div>
+                    <span className='text-lg font-medium'>Rating: </span>
+                    <span>{rating} (based on 2,370 reviews) </span>
+                </div>
+                <h3 className='text-lg font-medium'>Key Features</h3>
             </div>
         </div>
     )
