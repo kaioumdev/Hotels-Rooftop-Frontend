6cc780f1ade263cd8808e536deb083fd35092fb5
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-03 17:14:44 +0600

    (login): complete login page styles

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
     return (
-        <div>Login</div>
+        <div className='max-w-sm bg-white mx-auto p-8 mt-36'>
+            <h2 className='text-2xl font-semibold pt-5'>Please Login</h2>
+            <form className='space-y-5 max-w-sm mx-auto pt-8'>
+                <input type="email" value={email}
+                    placeholder='Email Address'
+                    required
+                    className='w-full bg-bgPrimary focus:outline-none px-5 py-3'
+                />
+                <input type="password" value={password}
+                    placeholder='Password'
+                    required
+                    className='w-full bg-bgPrimary focus:outline-none px-5 py-3'
+                />
+                {
+                    message && <p className='text-red-500'>{message}</p>
+                }
+                <button className='w-full mt-5 bg-primary hover:bg-indigo-500 text-white font-medium py-3 rounded-md'>Login</button>
+            </form>
+            <p className='my-5 text-center'>Don't have a account? <Link className='text-red-700 italic' to="/register">Register</Link>here.</p>
+        </div>
     )
 }
 
