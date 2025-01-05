75b2179d909c5fcf4cbeb33100a6f1b18ecfa109
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-05 15:45:14 +0600

    remove older or unusual code from authSlice

--- src/redux/features/auth/authSlice.js
+++ src/redux/features/auth/authSlice.js
@@ -1,43 +1,4 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
 import { createSlice } from "@reduxjs/toolkit";
-
-// const isTokenPresentInCookies = () => {
-//     const token = document.cookie.split(';').find(cookie => cookie.trim().startsWith('token="'));
-//     return !!token
-// };
-
-// const loadUserFromLocalStorage = () => {
-//     try {
-//         const serializedState = localStorage.getItem('user');
-//         if (serializedState === null) {
-//             return { user: JSON.parse(serializedState) }
-//         }
-//         return { user: JSON.parse(serializedState) }
-//     } catch (error) {
-//         console.error('Error loading user from localStorage:', error);
-//         return { user: null }
-//     }
-// }
-
-// const initialState = loadUserFromLocalStorage();
-// const authSlice = createSlice({
-//     name: "auth",
-//     initialState,
-//     reducers: {
-//         setUser: (state, action) => {
-//             state.user = action.payload.user;
-//             localStorage.setItem('user', JSON.stringify(state.user));
-//         },
-//         logOut: (state) => {
-//             state.user = null;
-//             localStorage.removeItem('user');
-//         }
-//     }
-// });
-
-// export const { setUser, logOut } = authSlice.actions;
-// export default authSlice.reducer;
 
 const isTokenPresentInCookies = () => {
     const token = document.cookie.split(';').find(cookie => cookie.trim().startsWith('token='));
