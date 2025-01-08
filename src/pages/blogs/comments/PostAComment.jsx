e4b8c22ac328c0713cc636d8d44f3da0829e71ba
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-08 12:09:05 +0600

    get comment info successfully

--- src/pages/blogs/comments/PostAComment.jsx
+++ src/pages/blogs/comments/PostAComment.jsx
@@ -16,12 +16,18 @@ const PostAComment = () => {
             alert("Please login to comment on this post");
             navigate("/login");
             return;
+        };
+        const newComment = {
+            comment: comment,
+            user: user._id,
+            postId: id
         }
+        console.log(newComment);
     }
     return (
         <div className='mt-8'>
             <h3 className='text-lg font-medium mb-8'>Leave a Comment</h3>
-            <form>
+            <form onSubmit={handlePostComment}>
                 <textarea name="text" value={comment} onChange={(e) => setComment(e.target.value)} cols="30" rows="10" placeholder='Share your opinion about this post....'
                     className='w-full bg-bgPrimary focus:outline-none p-5' id=""></textarea>
                 <button type='submit' className='w-full bg-primary hover:bg-indigo-500 text-white font-medium py-3 rounded-md'>Submit</button>
