b907ca264aa48ffe22daf9bb62190057f67a7a29
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-08 20:59:31 +0600

    styles adminLayout page

--- src/pages/admin/AdminLayout.jsx
+++ src/pages/admin/AdminLayout.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const AdminLayout = () => {
     return (
-        <div>AdminLayout</div>
+        <div className='container mx-auto flex flex-col md:flex-row gap-4 items-start justify-start'>
+            <header className='lg:w-1/5 sm:2/5 w-full'>
+                Admin Navigation
+            </header>
+            <main className='p-8 bg-white w-full'>
+                <p>For Admin Content</p>
+                <Outlet></Outlet>
+            </main>
+        </div>
     )
 }
 
