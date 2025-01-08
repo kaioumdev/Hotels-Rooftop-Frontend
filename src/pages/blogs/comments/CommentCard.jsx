96e0f8ce0a131a5c6c8a9d473efb1c1f90103d02
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-08 17:11:03 +0600

    styles comment section

--- src/pages/blogs/comments/CommentCard.jsx
+++ src/pages/blogs/comments/CommentCard.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { formatDate } from '../../../utils/formateDate';
 import commentIcon from '../../../assets/commentor.png';
 import PostAComment from './PostAComment';
+import { useSelector } from 'react-redux';
 
 
 const CommentCard = ({ comments }) => {
+    console.log(comments)
+    const user = useSelector((state) => state.auth.user);
     return (
         <div className='my-6 bg-white p-8'>
             <div>
@@ -14,8 +17,8 @@ const CommentCard = ({ comments }) => {
                         <div>
                             {
                                 comments.map((comment, index) => (
-                                    <div key={index}>
-                                        <div>
+                                    <div key={index} className='mt-4'>
+                                        <div className='flex gap-4 items-center'>
                                             <img src={commentIcon} alt="" className='h-14' />
                                             <div>
                                                 <p className='text-lg font-medium underline capitalize underline-offset-4 text-blue-400'>{comment?.user?.username}</p>
