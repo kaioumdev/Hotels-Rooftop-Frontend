fb7ea67ea971ca07385515d9549e11ac1d9c3238
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-09 12:08:54 +0600

    implements conditional rendering dashboard button

--- src/pages/admin/AdminNavigation.jsx
+++ src/pages/admin/AdminNavigation.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import AdminImg from "../../assets/admin.png";
+import { NavLink } from 'react-router-dom';
 
 const AdminNavigation = () => {
     return (
-        <div className='space-y-5 bg-white p-8 md:h-[calc(100vh-98px)] flex flex-col justify-center'>
+        <div className='space-y-5 bg-white p-8 md:h-[calc(100vh-98px)] flex flex-col justify-between'>
             <div className='mb-5'>
                 <img src={AdminImg} alt="AdminImg" className='size-14' />
                 <p className='font-semibold'>Admin</p>
             </div>
+            <hr />
+            <ul>
+                <li>
+                    <NavLink to="/dashboard" className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-black"}>Dashboard</NavLink>
+                </li>
+            </ul>
         </div>
     )
 }
