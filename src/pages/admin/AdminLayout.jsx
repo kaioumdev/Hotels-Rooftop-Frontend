283c9490f6b317ac4b38c171ee21d967b34daf76
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-09 11:31:07 +0600

    add adminNavigation at adminLayout page

--- src/pages/admin/AdminLayout.jsx
+++ src/pages/admin/AdminLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import AdminNavigation from './AdminNavigation';
 
 const AdminLayout = () => {
     return (
         <div className='container mx-auto flex flex-col md:flex-row gap-4 items-start justify-start'>
             <header className='lg:w-1/5 sm:2/5 w-full'>
-                Admin Navigation
+                <AdminNavigation></AdminNavigation>
             </header>
             <main className='p-8 bg-white w-full'>
                 <p>For Admin Content</p>
