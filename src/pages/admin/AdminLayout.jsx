af5d73ff313616734db54a30fc89248976252c91
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-09 11:34:06 +0600

    if not user then navigate to the login page

--- src/pages/admin/AdminLayout.jsx
+++ src/pages/admin/AdminLayout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 import AdminNavigation from './AdminNavigation';
+import { useSelector } from 'react-redux';
 
 const AdminLayout = () => {
+    const { user } = useSelector((state) => state.auth);
+    if (!user || user.role !== 'admin') {
+        return <Navigate to="/login"></Navigate>
+    }
     return (
         <div className='container mx-auto flex flex-col md:flex-row gap-4 items-start justify-start'>
             <header className='lg:w-1/5 sm:2/5 w-full'>
