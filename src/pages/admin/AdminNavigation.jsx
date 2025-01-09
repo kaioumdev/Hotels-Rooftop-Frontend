991a9c1cd10763d063a7c95bf2e204a77a19dcbe
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-09 11:46:26 +0600

    styles adminNavigation page

--- src/pages/admin/AdminNavigation.jsx
+++ src/pages/admin/AdminNavigation.jsx
@@ -3,11 +3,10 @@ import AdminImg from "../../assets/admin.png";
 
 const AdminNavigation = () => {
     return (
-        <div>
-            <div>
-                <header>
-                    <img src={AdminImg} alt="AdminImg" className='size-14' />
-                </header>
+        <div className='space-y-5 bg-white p-8 md:h-[calc(100vh-98px)] flex flex-col justify-center'>
+            <div className='mb-5'>
+                <img src={AdminImg} alt="AdminImg" className='size-14' />
+                <p className='font-semibold'>Admin</p>
             </div>
         </div>
     )
