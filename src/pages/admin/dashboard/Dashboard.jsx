dd31a5576901de1073e327274876e7cf52fbcef4
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-09 22:22:51 +0600

    styles admin dashboard welcome text

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Dashboard = () => {
+    const { user } = useSelector((state) => state.auth);
     return (
-        <div>Dashboard</div>
+        <div>
+            <div>
+                <h1>Hi, {user?.username}</h1>
+                <p>Welcome to the admin dashboard</p>
+                <p>Here you can manage your hotel's posts, manage rooms, and other administrative tasks.</p>
+            </div>
+        </div>
     )
 }
 
