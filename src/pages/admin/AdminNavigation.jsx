c39f97f15008f88e099c9d05201829aaa3f9c9c7
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-09 15:45:25 +0600

    create logOut section at adminNavigation page

--- src/pages/admin/AdminNavigation.jsx
+++ src/pages/admin/AdminNavigation.jsx
@@ -5,25 +5,31 @@ import { NavLink } from 'react-router-dom';
 const AdminNavigation = () => {
     return (
         <div className='space-y-5 bg-white p-8 md:h-[calc(100vh-98px)] flex flex-col justify-between'>
-            <div className='mb-5'>
-                <img src={AdminImg} alt="AdminImg" className='size-14' />
-                <p className='font-semibold'>Admin</p>
+            <div>
+                <div className='mb-5'>
+                    <img src={AdminImg} alt="AdminImg" className='size-14' />
+                    <p className='font-semibold'>Admin</p>
+                </div>
+                <hr />
+                <ul>
+                    <li>
+                        <NavLink to="/dashboard" className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-black"}>Dashboard</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/add-new-post" className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-black"}>Add New Post</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/manage-items" className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-black"}>Manage Items</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/users" className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-black"}>Users</NavLink>
+                    </li>
+                </ul>
             </div>
-            <hr />
-            <ul>
-                <li>
-                    <NavLink to="/dashboard" className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-black"}>Dashboard</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/add-new-post" className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-black"}>Add New Post</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/manage-items" className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-black"}>Manage Items</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/users" className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-black"}>Users</NavLink>
-                </li>
-            </ul>
+            <div className='mb-3'>
+                <hr />
+                <button className='text-white bg-red-500 font-medium px-5 py-1 rounded-sm'>LogOut</button>
+            </div>
         </div>
     )
 }
