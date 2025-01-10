fd759468a79b13ad6927bf5b3a80da7df85b85b5
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-10 18:29:07 +0600

    install react-cons using users icons

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import { FiUsers } from 'react-icons/fi';
 
 const Dashboard = () => {
     const { user } = useSelector((state) => state.auth);
     return (
-        <div className='space-y-6'>
-            <div className='bg-bgPrimary p-5'>
-                <h1>Hi, {user?.username}</h1>
-                <p>Welcome to the admin dashboard</p>
-                <p>Here you can manage your hotel's posts, manage rooms, and other administrative tasks.</p>
-            </div>
+        <>
+            <div className='space-y-6'>
+                <div className='bg-bgPrimary p-5'>
+                    <h1>Hi, {user?.username}!</h1>
+                    <p>Welcome to the admin dashboard</p>
+                    <p>Here you can manage your hotel's posts, manage rooms, and other administrative tasks.</p>
+                </div>
 
-            {/* cards grid */}
-            <div>
-
+                {/* cards grid */}
+                <div>
+                    <div>
+                        <FiUsers className='size-8 text-indigo-600'></FiUsers>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
