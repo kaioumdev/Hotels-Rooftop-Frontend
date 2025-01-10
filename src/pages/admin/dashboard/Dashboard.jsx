98fa2fd3b0543ebdbf433389975b05497fca97be
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-10 18:07:22 +0600

    updated dashboard styles

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -4,11 +4,16 @@ import { useSelector } from 'react-redux'
 const Dashboard = () => {
     const { user } = useSelector((state) => state.auth);
     return (
-        <div>
-            <div>
+        <div className='space-y-6'>
+            <div className='bg-bgPrimary p-5'>
                 <h1>Hi, {user?.username}</h1>
                 <p>Welcome to the admin dashboard</p>
                 <p>Here you can manage your hotel's posts, manage rooms, and other administrative tasks.</p>
+            </div>
+
+            {/* cards grid */}
+            <div>
+
             </div>
         </div>
     )
