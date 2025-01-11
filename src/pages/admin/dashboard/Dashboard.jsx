ecb437f54f6391ea2da829bebb3e2b45d3175c0e
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-11 11:36:30 +0600

    successfully show the blog users dynamically

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -23,14 +23,13 @@ const Dashboard = () => {
                 {/* cards grid */}
                 <div className='flex flex-col md:flex-row justify-center gap-8 pt-8'>
                     <div className='bg-indigo-100 py-6 w-full rounded-sm space-y-2 flex flex-col items-center'>
-                        <FiUsers className='size-8 text-indigo-600'>
-                            <p>2 Users</p>
-                        </FiUsers>
+                        <FiUsers className='size-8 text-indigo-600'></FiUsers>
+                        <p>2 Users</p>
                     </div>
                     <div className='bg-red-100 py-6 w-full rounded-sm space-y-2 flex flex-col items-center'>
                         <FaBlog className='size-8 text-red-600'>
-                            <p>{blogs.length} Blogs</p>
                         </FaBlog>
+                        <p>{blogs.length} Blogs</p>
                     </div>
                     <div className='bg-lime-100 py-6 w-full rounded-sm space-y-2 flex flex-col items-center'>
                         <FaRegComment className='size-8 text-lime-600'>
