3c9bf43da91792dbf925214498fa35a3d1ee0803
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-11 12:27:39 +0600

    create formatData for showing charts & graph

--- src/pages/admin/dashboard/BlogsCart.jsx
+++ src/pages/admin/dashboard/BlogsCart.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import { formatDate } from '../../../utils/formateDate'
+
+const formatData = (blogs) => {
+    return blogs.map(blog => ({
+        name: formatDate(blog.createAt),
+        post: blog.title.length,
+        pv: blog.pageView || 0,
+        amt: blog.amt || 0,
+    }))
+}
 
 const BlogsCart = ({ blogs }) => {
     return (
