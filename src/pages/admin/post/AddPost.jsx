6089e02369fc48e82fd5b8e82156102c5c033d50
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-11 18:17:29 +0600

    complete description section

--- src/pages/admin/post/AddPost.jsx
+++ src/pages/admin/post/AddPost.jsx
@@ -25,20 +25,30 @@ const AddPost = () => {
                     {/* right side */}
                     <div className='md:w-1/3 w-full border p-5 space-y-5'>
                         <p className='text-xl font-semibold'>Choose Blog Format</p>
+
                         {/* images */}
                         <div className='space-y-4'>
                             <label className='font-semibold'>Blog Cover:</label>
-                            <input type="text" value={coverImg} onChange={(e) => setTitle(e.target.value)}
+                            <input type="text" value={coverImg} onChange={(e) => setCoverImg(e.target.value)}
                                 className='w-full inline-block bg-bgPrimary focus:outline-none px-5 py-3'
                                 placeholder='https://unsplash.com/photos/cover-photo-of-blog1.png' required
                             />
                         </div>
+
                         {/* category  */}
                         <div className='space-y-4'>
                             <label className='font-semibold'>Category:</label>
-                            <input type="text" value={coverImg} onChange={(e) => setTitle(e.target.value)}
+                            <input type="text" value={category} onChange={(e) => setCategory(e.target.value)}
                                 className='w-full inline-block bg-bgPrimary focus:outline-none px-5 py-3'
                                 placeholder='Rooftop/Travel/Nature' required
+                            />
+                        </div>
+                        {/* meta description */}
+                        <div className='space-y-4'>
+                            <label className='font-semibold'>Meta Description:</label>
+                            <textarea type="text" cols={4} rows={4} value={metaDescription} onChange={(e) => setMetaDescription(e.target.value)}
+                                className='w-full inline-block bg-bgPrimary focus:outline-none px-5 py-3'
+                                placeholder='Write your blog meta description' required
                             />
                         </div>
                     </div>
