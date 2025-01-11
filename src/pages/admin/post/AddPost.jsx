1a568f7817acb5e928812c7aefffd9a54cdc1130
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-11 16:15:50 +0600

    styles addPost complete

--- src/pages/admin/post/AddPost.jsx
+++ src/pages/admin/post/AddPost.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddPost = () => {
+    const [title, setTitle] = useState('');
     return (
-        <div>AddPost</div>
+        <div className='bg-white md:p-8 p-2'>
+            <h2 className='text-2xl font-semibold'>Create A New Post</h2>
+            <form className='space-y-5 pt-8'>
+                <div className='space-y-4'>
+                    <label className='font-semibold text-xl'>Blog Title:</label>
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}
+                        className='w-full inline-block bg-bgPrimary focus:outline-none px-5 py-3'
+                        placeholder='Ex: Marina del Rey Marriott..' required
+                    />
+                </div>
+            </form>
+        </div>
     )
 }
 
