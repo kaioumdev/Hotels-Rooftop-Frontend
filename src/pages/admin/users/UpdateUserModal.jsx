cafdaa660d1337e143e397b41ca0cb6aff2ff50c
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-13 20:34:07 +0600

    show user & role dynamically

--- src/pages/admin/users/UpdateUserModal.jsx
+++ src/pages/admin/users/UpdateUserModal.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const UpdateUserModal = ({ user, onClose, }) => {
+const UpdateUserModal = ({ user, onClose, onRoleUpdate }) => {
+    const [role, setRole] = useState(user?.role);
     return (
         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
             <div className='bg-white p-4 rounded shadow-lg w-1/3'>
                 <h2 className='text-xl mb-4'>Edit User</h2>
                 <div className='mb-4 space-y-4'>
-                    <label>Email</label>
+                    <label className='block text-sm font-medium text-gray-700'>Email</label>
                     <input type="text" value={user?.email} readOnly className='mt-1 bg-bgPrimary block shadow-sm sm:text-sm border-gray-300 rounded-md py-1.5 px-5 focus:outline-none' />
+                </div>
+                <div className='mb-4 space-y-4'>
+                    <select value={role} onChange={(e) => setRole(e.target.value)}>
+                        <option value="user">User</option>
+                        <option value="admin">Admin</option>
+                    </select>
                 </div>
             </div>
         </div>
