be7d561712d3942282db8c420a6bb639e53605c4
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-13 19:46:24 +0600

    create updateUserModal page

--- src/pages/admin/users/ManageUser.jsx
+++ src/pages/admin/users/ManageUser.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 const ManageUser = () => {
     const [selectedUser, setSelectedUser] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const { data, error, isLoading, refetch } = useGetUserQuery();
     const [deleteUser] = useDeleteUserMutation();
     const navigate = useNavigate();
