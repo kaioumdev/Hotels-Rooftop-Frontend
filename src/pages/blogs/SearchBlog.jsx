33181e277dc0bd19b8f95dca3c82beb4483ca0f8
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-14 17:36:28 +0600

    make search functionality at the center of the page

--- src/pages/blogs/SearchBlog.jsx
+++ src/pages/blogs/SearchBlog.jsx
@@ -7,7 +7,7 @@ const SearchBlog = ({ search, handleSearchChange, handleSearch }) => {
         }
     }
     return (
-        <div className='w-full flex'>
+        <div className='w-full flex md:w-6/12 mx-auto'>
             <input type="text"
                 placeholder='Hotels with Rooftop Pool Near...'
                 className='py-2 px-4 mr-5 w-full bg-[#f7f8f9] focus:outline-none focus:border'
@@ -15,7 +15,7 @@ const SearchBlog = ({ search, handleSearchChange, handleSearch }) => {
                 onChange={handleSearchChange}
                 onKeyPress={handleKeyPress}
             />
-            <button onClick={handleSearch} className='bg-[#1E73BE] px-4 py-2 text-white'>Search</button>
+            <button onClick={handleSearch} className='bg-[#1E73BE] px-4 py-2 text-white rounded'>Search</button>
         </div>
     )
 }
