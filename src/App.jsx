b87d0649cd3f85878acae7843ebed7d0f6da3777
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-11 17:51:30 +0600

    feat(updated): updated app page to set the project height to show footer at the bottom of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
-    <>
-      <Navbar></Navbar>
-      <Outlet></Outlet>
-      <Footer></Footer>
-    </>
-  )
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
