256debe4e81f112d2c1e4010db8e0cf8b624499d
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-12 21:09:19 +0600

    remove aboutUs from home page successfully

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from './Hero';
 import Categories from './Categories';
 import FeaturedPosts from './FeaturedPosts';
-import LatestPosts from './LatestPosts';
 import AboutUs from './AboutUs';
 import NewsletterSignup from './Newsletter';
 import Blogs from '../blogs/Blogs';
@@ -14,8 +13,7 @@ const Home = () => {
             <FeaturedPosts />
             <Categories />
             <Blogs></Blogs>
-            {/* <AboutUs />
-            <NewsletterSignup /> */}
+            <NewsletterSignup />
         </div>
     );
 };
